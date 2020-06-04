a180567af9d79eed6e25d8779307cbab2ad76f28
tushardimble <dimbletushar7004@gmail.com>
2020-06-04 12:58:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -44,26 +44,56 @@
     }else if($intent == "Raising Service Request"){
       $mobile_number =   $requestDecode -> queryResult -> parameters -> mobile_number;  
       // If Mobile Number is not blank then check Customer is exist or not in our system by calling Voltas API
-      $message = "Hi Tushar, we have sent an OTP to your mobile number";
+      $curl = curl_init();
 
-      // Send OTP on Mobile Number
-      // $curl = curl_init();
-      // $url = "http://2factor.in/API/V1/066901d9-a62e-11ea-9fa5-0200cd936042/SMS/".$mobile_number."/".$otp;
-      // curl_setopt_array($curl, array(
-      //   CURLOPT_URL => $url,
-      //   CURLOPT_RETURNTRANSFER => true,
-      //   CURLOPT_ENCODING => "",
-      //   CURLOPT_MAXREDIRS => 10,
-      //   CURLOPT_TIMEOUT => 0,
-      //   CURLOPT_FOLLOWLOCATION => true,
-      //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      //   CURLOPT_CUSTOMREQUEST => "POST",
-      // ));
+      curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://qavcare.voltasworld.com/siebel/v1.0/service/UPBGAccountSRQueryAPI/QuerySRWithContact",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS =>"{\r\n    \"body\": {\r\n        \"Phone Number\": \"1190000005\"\r\n    }\r\n}\r\n",
+        CURLOPT_HTTPHEADER => array(
+          "Authorization: Basic Q09OTkVRVDpVSSgzMzAyMzB0",
+          "Content-Type: application/json"
+        ),
+      ));
 
-      // $response = curl_exec($curl);
-      // $err = curl_error($curl);
+      $aAccountInforesponse = curl_exec($curl);
 
-      // curl_close($curl);
+      curl_close($curl);
+      $user_account_info = json_decode($aAccountInforesponse,true);
+
+      $iCount_Account = $user_account_info['Count_Account'];
+
+      if($iCount_Account != 0){
+
+        // Send OTP on Mobile Number
+        // $curl = curl_init();
+        // $url = "http://2factor.in/API/V1/066901d9-a62e-11ea-9fa5-0200cd936042/SMS/".$mobile_number."/".$otp;
+        // curl_setopt_array($curl, array(
+        //   CURLOPT_URL => $url,
+        //   CURLOPT_RETURNTRANSFER => true,
+        //   CURLOPT_ENCODING => "",
+        //   CURLOPT_MAXREDIRS => 10,
+        //   CURLOPT_TIMEOUT => 0,
+        //   CURLOPT_FOLLOWLOCATION => true,
+        //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //   CURLOPT_CUSTOMREQUEST => "POST",
+        // ));
+
+        // $response = curl_exec($curl);
+        // $err = curl_error($curl);
+
+        // curl_close($curl);
+        $message = "Hi ".$user_account_info['Count_Account'].", we have sent an OTP to your mobile number";
+
+      }else{
+        // Account not found
+      }
     }
     $data = array (
       'fulfillmentText' => $message

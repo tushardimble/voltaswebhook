e956b4725331187958a2289e597b7a0e610b4d12
tushardimble <dimbletushar7004@gmail.com>
2020-06-04 12:19:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,38 +8,7 @@
     $intent     	= $requestDecode  ->  queryResult ->  intent  ->  displayName;
     $userQueryText  = $requestDecode  ->  queryResult ->  queryText;
 
-    if($intent == "customer"){
-    	$current_time 	= 	date("H:i");
-    	$current_time   =   strtotime($current_time);
-
-    	$start_time  	= 	"9:00";
-    	$start_time    	=   strtotime($start_time);
-
-    	$end_time  		= 	"18:00";
-    	$end_time    	=   strtotime($end_time);
-    	// "current time ". $current_time ."<br /> Start Time ". $start_time. " <br /> End Time ".$end_time;exit;
-    	if($current_time > $start_time && $current_time > $end_time){
-    		$demo_request 	= "getButtonText('1')";
-    		$ticket_status 	= "getButtonText('2')";
-    		$raise_cmt 		= "getButtonText('3')";
-    		$comp_warranty 	= "getButtonText('4')";
-
-    		$message = "It is our pleasure to have you as customer. I can help you in doing the following.<br />  <button class='btn-sm btn-primary'  onclick=$demo_request>Demo / Installation Request</button><br/><button class='btn-sm btn-primary' onclick=$ticket_status>Status of Tickets</button><br/><button class='btn-sm btn-primary' onclick=$raise_cmt>Raise a Complaint</button><br><button class='btn-sm btn-primary' onclick=$comp_warranty>Comprehensive warranty</button>";
-    	}else{
-    		$message = "Sorry, Our Operational time is from 9 AM to 6 PM. You can share you phone number and we will give a call.";
-    		
-    	}
-    	
-    }else if($intent == "customer_option" || $intent == "connect_to_agent - yes"){
-    	// Check is Agent is Available
-    	$sAgentAvailability = "yes";
-    	$user_name     = $requestDecode -> queryResult -> parameters -> user_name;
-    	if($sAgentAvailability == "yes"){
-    		$message = $user_name." You have been connected to an Agent";
-    	}else{
-    		$message = "Sorry ".$user_name." it seems our agent busy. Can you try after sometime. Thank you for our co-operation";
-    	}
-    }else if($intent == "SRstatus"){
+    if($intent == "SRstatus"){
         // Here We integrate Voltas API to check SR Status
         $ticket_number = $requestDecode -> queryResult -> parameters -> ticketno;
         if($ticket_number != ""){
@@ -72,6 +41,29 @@
             }
             
         }
+    }else if($intent == "Raising Service Request"){
+      $mobile_number =   $requestDecode -> queryResult -> parameters -> mobile_number;  
+      // If Mobile Number is not blank then check Customer is exist or not in our system by calling Voltas API
+      $msg = "Hi Tushar, we have sent an OTP to your mobile number";
+
+      // Send OTP on Mobile Number
+      // $curl = curl_init();
+      // $url = "http://2factor.in/API/V1/066901d9-a62e-11ea-9fa5-0200cd936042/SMS/".$mobile_number."/".$otp;
+      // curl_setopt_array($curl, array(
+      //   CURLOPT_URL => $url,
+      //   CURLOPT_RETURNTRANSFER => true,
+      //   CURLOPT_ENCODING => "",
+      //   CURLOPT_MAXREDIRS => 10,
+      //   CURLOPT_TIMEOUT => 0,
+      //   CURLOPT_FOLLOWLOCATION => true,
+      //   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      //   CURLOPT_CUSTOMREQUEST => "POST",
+      // ));
+
+      // $response = curl_exec($curl);
+      // $err = curl_error($curl);
+
+      // curl_close($curl);
     }
     $data = array (
       'fulfillmentText' => $message

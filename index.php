42443b2b74ac090db89bf9a6fc4c91a097ad9188
tushardimble <dimbletushar7004@gmail.com>
2020-06-05 14:15:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
   $password   = "";
   $dbname     = "voltas_webhook";
   // Connect with Db 
-  try{
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected";exit;
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
+//   try{
+//     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+//     // set the PDO error mode to exception
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     //echo "Connected";exit;
+//   } catch(PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+//   }
 
 	$request    	= file_get_contents('php://input');
   $requestDecode  = json_decode($request);
